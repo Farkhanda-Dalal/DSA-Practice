b475730bb1a84ad7037328ad3b8783a4d6f500fd
Farkhanda-Dalal <125860512+Farkhanda-Dalal@users.noreply.github.com>
2024-09-17 21:42:14 +0530

    Absolute of a no

---
+++ Mathematics/Absoulute.java
@@ -0,0 +1,10 @@
+public class Absoulute {
+    public static void main(String[] args) {
+        int n=0;
+        System.out.println("Using Math func: "+Math.abs(n));
+        System.out.println("Using Multiplication with -1 :"+n*-1);
+        System.out.println("Using subtraction with (n*2) :"+(n-(n*2)));
+
+        
+    }
+}
